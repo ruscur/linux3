169ccaf487b369161dee753846003bc0bb9e1205
Nayna Jain <nayna@linux.ibm.com>
2019-10-19 14:06:10 -0400

    powerpc: detect the secure boot mode of the system
    This patch defines a function to detect the secure boot state of a
    PowerNV system.
    
    The PPC_SECURE_BOOT config represents the base enablement of secure boot
    for powerpc.
    
    Signed-off-by: Nayna Jain <nayna@linux.ibm.com>

---
+++ arch/powerpc/kernel/secure_boot.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2019 IBM Corporation
+ * Author: Nayna Jain
+ */
+#include <linux/types.h>
+#include <linux/of.h>
+#include <asm/secure_boot.h>
+
+bool is_ppc_secureboot_enabled(void)
+{
+	struct device_node *node;
+	bool enabled = false;
+
+	node = of_find_compatible_node(NULL, NULL, "ibm,secvar-v1");
+	if (!of_device_is_available(node)) {
+		pr_err("Cannot find secure variable node in device tree; failing to secure state\n");
+		goto out;
+	}
+
+	/*
+	 * secureboot is enabled if os-secure-enforcing property exists,
+	 * else disabled.
+	 */
+	enabled = of_property_read_bool(node, "os-secure-enforcing");
+
+out:
+	pr_info("Secure boot mode %s\n", enabled ? "enabled" : "disabled");
+	return enabled;
+}
