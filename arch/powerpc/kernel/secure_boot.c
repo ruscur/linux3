9f269f78b63e224440b6f689eaeaaf46b0d15245
Nayna Jain <nayna@linux.ibm.com>
2019-10-19 14:06:12 -0400

    powerpc: detect the trusted boot state of the system
    While secure boot permits only properly verified signed kernels to be
    booted, trusted boot takes a measurement of the kernel image prior to
    boot that can be subsequently compared against good known values via
    attestation services.
    
    This patch reads the trusted boot state of a PowerNV system. The state
    is used to conditionally enable additional measurement rules in the IMA
    arch-specific policies.
    
    Signed-off-by: Nayna Jain <nayna@linux.ibm.com>

--- arch/powerpc/kernel/secure_boot.c
+++ arch/powerpc/kernel/secure_boot.c
@@ -7,6 +7,17 @@
 #include <linux/of.h>
 #include <asm/secure_boot.h>
 
+static struct device_node *get_ppc_fw_sb_node(void)
+{
+	static const struct of_device_id ids[] = {
+		{ .compatible = "ibm,secureboot-v1", },
+		{ .compatible = "ibm,secureboot-v2", },
+		{},
+	};
+
+	return of_find_matching_node(NULL, ids);
+}
+
 bool is_ppc_secureboot_enabled(void)
 {
 	struct device_node *node;
@@ -26,5 +37,18 @@ bool is_ppc_secureboot_enabled(void)
 
 out:
 	pr_info("Secure boot mode %s\n", enabled ? "enabled" : "disabled");
+	return enabled;
+}
+
+bool is_ppc_trustedboot_enabled(void)
+{
+	struct device_node *node;
+	bool enabled = false;
+
+	node = get_ppc_fw_sb_node();
+	enabled = of_property_read_bool(node, "trusted-enabled");
+
+	pr_info("Trusted boot mode %s\n", enabled ? "enabled" : "disabled");
+
 	return enabled;
 }
