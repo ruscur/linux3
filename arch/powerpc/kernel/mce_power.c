17c5849ceac2158eb54eda9d9d34893fd1a37e7b
Jordan Niethe <jniethe5@gmail.com>
2019-11-26 16:21:40 +1100

    powerpc: Add prefix support to mce_find_instr_ea_and_pfn()
    mce_find_instr_ea_and_pfn analyses an instruction to determine the
    effective address that caused the machine check. Update this to load and
    pass the suffix to analyse_instr for prefixed instructions.
    
    Signed-off-by: Jordan Niethe <jniethe5@gmail.com>

--- arch/powerpc/kernel/mce_power.c
+++ arch/powerpc/kernel/mce_power.c
@@ -365,7 +365,7 @@ static int mce_find_instr_ea_and_phys(struct pt_regs *regs, uint64_t *addr,
 	 * in real-mode is tricky and can lead to recursive
 	 * faults
 	 */
-	int instr;
+	int instr, sufx = 0;
 	unsigned long pfn, instr_addr;
 	struct instruction_op op;
 	struct pt_regs tmp = *regs;
@@ -374,7 +374,9 @@ static int mce_find_instr_ea_and_phys(struct pt_regs *regs, uint64_t *addr,
 	if (pfn != ULONG_MAX) {
 		instr_addr = (pfn << PAGE_SHIFT) + (regs->nip & ~PAGE_MASK);
 		instr = *(unsigned int *)(instr_addr);
-		if (!analyse_instr(&op, &tmp, instr, 0)) {
+		if (IS_PREFIX(instr))
+			sufx = *(unsigned int *)(instr_addr + 4);
+		if (!analyse_instr(&op, &tmp, instr, sufx)) {
 			pfn = addr_to_pfn(regs, op.ea);
 			*addr = op.ea;
 			*phys_addr = (pfn << PAGE_SHIFT);
