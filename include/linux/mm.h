b858ee957bcc3735ff4400ac0134f37cce334900
John Hubbard <jhubbard@nvidia.com>
2019-12-09 14:53:22 -0800

    mm: devmap: refactor 1-based refcounting for ZONE_DEVICE pages
    An upcoming patch changes and complicates the refcounting and
    especially the "put page" aspects of it. In order to keep
    everything clean, refactor the devmap page release routines:
    
    * Rename put_devmap_managed_page() to page_is_devmap_managed(),
      and limit the functionality to "read only": return a bool,
      with no side effects.
    
    * Add a new routine, put_devmap_managed_page(), to handle checking
      what kind of page it is, and what kind of refcount handling it
      requires.
    
    * Rename __put_devmap_managed_page() to free_devmap_managed_page(),
      and limit the functionality to unconditionally freeing a devmap
      page.
    
    This is originally based on a separate patch by Ira Weiny, which
    applied to an early version of the put_user_page() experiments.
    Since then, Jérôme Glisse suggested the refactoring described above.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Suggested-by: Jérôme Glisse <jglisse@redhat.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -952,9 +952,10 @@ static inline bool is_zone_device_page(const struct page *page)
 #endif
 
 #ifdef CONFIG_DEV_PAGEMAP_OPS
-void __put_devmap_managed_page(struct page *page);
+void free_devmap_managed_page(struct page *page);
 DECLARE_STATIC_KEY_FALSE(devmap_managed_key);
-static inline bool put_devmap_managed_page(struct page *page)
+
+static inline bool page_is_devmap_managed(struct page *page)
 {
 	if (!static_branch_unlikely(&devmap_managed_key))
 		return false;
@@ -963,7 +964,6 @@ static inline bool put_devmap_managed_page(struct page *page)
 	switch (page->pgmap->type) {
 	case MEMORY_DEVICE_PRIVATE:
 	case MEMORY_DEVICE_FS_DAX:
-		__put_devmap_managed_page(page);
 		return true;
 	default:
 		break;
@@ -971,7 +971,14 @@ static inline bool put_devmap_managed_page(struct page *page)
 	return false;
 }
 
+bool put_devmap_managed_page(struct page *page);
+
 #else /* CONFIG_DEV_PAGEMAP_OPS */
+static inline bool page_is_devmap_managed(struct page *page)
+{
+	return false;
+}
+
 static inline bool put_devmap_managed_page(struct page *page)
 {
 	return false;
@@ -1028,8 +1035,10 @@ static inline void put_page(struct page *page)
 	 * need to inform the device driver through callback. See
 	 * include/linux/memremap.h and HMM for details.
 	 */
-	if (put_devmap_managed_page(page))
+	if (page_is_devmap_managed(page)) {
+		put_devmap_managed_page(page);
 		return;
+	}
 
 	if (put_page_testzero(page))
 		__put_page(page);
