5ae667ce17c6a8fbbe482a0ffc08401dcc1a2919
Shengjiu Wang <shengjiu.wang@nxp.com>
2020-02-18 14:39:35 +0800

    ASoC: fsl_asrc: Move common definition to fsl_asrc_common
    There is a new ASRC included in i.MX serial platform, there
    are some common definition can be shared with each other.
    So move the common definition to a separate header file.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

---
+++ sound/soc/fsl/fsl_asrc_common.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2019 NXP
+ *
+ */
+
+#ifndef _FSL_ASRC_COMMON_H
+#define _FSL_ASRC_COMMON_H
+
+/* directions */
+#define IN	0
+#define OUT	1
+
+enum asrc_pair_index {
+	ASRC_INVALID_PAIR = -1,
+	ASRC_PAIR_A = 0,
+	ASRC_PAIR_B = 1,
+	ASRC_PAIR_C = 2,
+};
+
+#endif /* _FSL_ASRC_COMMON_H */
