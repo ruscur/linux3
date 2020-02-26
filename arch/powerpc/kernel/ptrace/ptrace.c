69996c39f74dfb45be19d2322a9f2c37ab428410
Christophe Leroy <christophe.leroy@c-s.fr>
2020-02-26 11:54:03 +0000

    powerpc/ptrace: move ptrace_triggered() into hw_breakpoint.c
    ptrace_triggered() is declared in asm/hw_breakpoint.h and
    only needed when CONFIG_HW_BREAKPOINT is set, so move it
    into hw_breakpoint.c
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>

--- arch/powerpc/kernel/ptrace/ptrace.c
+++ arch/powerpc/kernel/ptrace/ptrace.c
@@ -29,24 +29,6 @@
 
 #include "ptrace-decl.h"
 
-#ifdef CONFIG_HAVE_HW_BREAKPOINT
-void ptrace_triggered(struct perf_event *bp,
-		      struct perf_sample_data *data, struct pt_regs *regs)
-{
-	struct perf_event_attr attr;
-
-	/*
-	 * Disable the breakpoint request here since ptrace has defined a
-	 * one-shot behaviour for breakpoint exceptions in PPC64.
-	 * The SIGTRAP signal is generated automatically for us in do_dabr().
-	 * We don't have to do anything about that here
-	 */
-	attr = bp->attr;
-	attr.disabled = true;
-	modify_user_hw_breakpoint(bp, &attr);
-}
-#endif /* CONFIG_HAVE_HW_BREAKPOINT */
-
 /*
  * Called by kernel/ptrace.c when detaching..
  *
