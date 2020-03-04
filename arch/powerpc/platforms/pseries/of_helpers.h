e4a6a30b1b326a0bd7fd9f9eccf71bb948ca241e
Pingfan Liu <kernelfans@gmail.com>
2020-03-04 16:47:29 +0800

    powerpc/of: split out new_property() for reusing
    Splitting out new_property() for coming reusing and moving it to
    of_helpers.c.
    
    Also do some coding style cleanup.
    
    Signed-off-by: Pingfan Liu <kernelfans@gmail.com>
    To: linuxppc-dev@lists.ozlabs.org
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Hari Bathini <hbathini@linux.ibm.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: Oliver O'Halloran <oohall@gmail.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Andrew Donnellan <ajd@linux.ibm.com>
    Cc: Christophe Leroy <christophe.leroy@c-s.fr>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Frank Rowand <frowand.list@gmail.com>
    Cc: kexec@lists.infradead.org

--- arch/powerpc/platforms/pseries/of_helpers.h
+++ arch/powerpc/platforms/pseries/of_helpers.h
@@ -4,6 +4,9 @@
 
 #include <linux/of.h>
 
+struct property *new_property(const char *name, const int length,
+		const unsigned char *value, struct property *last);
+
 struct device_node *pseries_of_derive_parent(const char *path);
 
 #endif /* _PSERIES_OF_HELPERS_H */
