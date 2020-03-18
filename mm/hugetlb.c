ead0c6650512ec12acc4d0b18780280d79e6f230
Mike Kravetz <mike.kravetz@oracle.com>
2020-03-18 15:06:33 -0700

    hugetlbfs: remove hugetlb_add_hstate() warning for existing hstate
    The routine hugetlb_add_hstate prints a warning if the hstate already
    exists.  This was originally done as part of kernel command line
    parsing.  If 'hugepagesz=' was specified more than once, the warning
            pr_warn("hugepagesz= specified twice, ignoring\n");
    would be printed.
    
    Some architectures want to enable all huge page sizes.  They would
    call hugetlb_add_hstate for all supported sizes.  However, this was
    done after command line processing and as a result hstates could have
    already been created for some sizes.  To make sure no warning were
    printed, there would often be code like:
            if (!size_to_hstate(size)
                    hugetlb_add_hstate(ilog2(size) - PAGE_SHIFT)
    
    The only time we want to print the warning is as the result of command
    line processing.  So, remove the warning from hugetlb_add_hstate and
    add it to the single arch independent routine processing "hugepagesz=".
    After this, calls to size_to_hstate() in arch specific code can be
    removed and hugetlb_add_hstate can be called without worrying about
    warning messages.
    
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2836,8 +2836,7 @@ static int __init hugetlb_init(void)
 		}
 
 		default_hstate_size = HPAGE_SIZE;
-		if (!size_to_hstate(default_hstate_size))
-			hugetlb_add_hstate(HUGETLB_PAGE_ORDER);
+		hugetlb_add_hstate(HUGETLB_PAGE_ORDER);
 	}
 	default_hstate_idx = hstate_index(size_to_hstate(default_hstate_size));
 	if (default_hstate_max_huge_pages) {
@@ -2875,7 +2874,6 @@ void __init hugetlb_add_hstate(unsigned int order)
 	unsigned long i;
 
 	if (size_to_hstate(PAGE_SIZE << order)) {
-		pr_warn("hugepagesz= specified twice, ignoring\n");
 		return;
 	}
 	BUG_ON(hugetlb_max_hstate >= HUGE_MAX_HSTATE);
@@ -2948,6 +2946,12 @@ static int __init hugepagesz_setup(char *s)
 	if (!arch_hugetlb_valid_size(size)) {
 		parsed_valid_hugepagesz = false;
 		pr_err("HugeTLB: unsupported hugepagesz %s\n", saved_s);
+		return 0;
+	}
+
+	if (size_to_hstate(size)) {
+		pr_warn("HugeTLB: hugepagesz %s specified twice, ignoring\n",
+			saved_s);
 		return 0;
 	}
 
