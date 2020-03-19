a1e15d7f3964e13f4a4e7701c03119e981272116
Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
2020-03-19 09:25:54 +0530

    powerpc/perf/callchain: Use __get_user_pages_fast in read_user_stack_slow
    read_user_stack_slow is called with interrupts soft disabled and it copies contents
    from the page which we find mapped to a specific address. To convert
    userspace address to pfn, the kernel now uses lockless page table walk.
    
    The kernel needs to make sure the pfn value read remains stable and is not released
    and reused for another process while the contents are read from the page. This
    can only be achieved by holding a page reference.
    
    One of the first approaches I tried was to check the pte value after the kernel
    copies the contents from the page. But as shown below we can still get it wrong
    
    CPU0                           CPU1
    pte = READ_ONCE(*ptep);
                                   pte_clear(pte);
                                   put_page(page);
                                   page = alloc_page();
                                   memcpy(page_address(page), "secret password", nr);
    memcpy(buf, kaddr + offset, nb);
                                   put_page(page);
                                   handle_mm_fault()
                                   page = alloc_page();
                                   set_pte(pte, page);
    if (pte_val(pte) != pte_val(*ptep))
    
    Hence switch to __get_user_pages_fast.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>

--- arch/powerpc/perf/callchain.c
+++ arch/powerpc/perf/callchain.c
@@ -107,46 +107,35 @@ perf_callchain_kernel(struct perf_callchain_entry_ctx *entry, struct pt_regs *re
  * On 64-bit we don't want to invoke hash_page on user addresses from
  * interrupt context, so if the access faults, we read the page tables
  * to find which page (if any) is mapped and access it directly.
+
+ * This should get called only if the PMU interrupt occurred while
+ * interrupts are soft-disabled, and there is no MMU hash table entry
+ * for the page . In this case we will get an -EFAULT return from
+ * __get_user_inatomic even if there is a valid Linux PTE for the page,
+ * since hash_page isn't reentrant.  Thus we have code here to read the
+ * Linux PTE and access the page via the kernel linear mapping.
  */
 static int read_user_stack_slow(void __user *ptr, void *buf, int nb)
 {
-	int ret = -EFAULT;
-	pgd_t *pgdir;
-	pte_t *ptep, pte;
-	unsigned shift;
+
 	unsigned long addr = (unsigned long) ptr;
 	unsigned long offset;
-	unsigned long pfn, flags;
+	struct page *page;
+	int nrpages;
 	void *kaddr;
 
-	pgdir = current->mm->pgd;
-	if (!pgdir)
-		return -EFAULT;
+	nrpages = __get_user_pages_fast(addr, 1, 1, &page);
+	if (nrpages == 1) {
+		kaddr = page_address(page);
 
-	local_irq_save(flags);
-	ptep = find_current_mm_pte(pgdir, addr, NULL, &shift);
-	if (!ptep)
-		goto err_out;
-	if (!shift)
-		shift = PAGE_SHIFT;
+		/* align address to page boundary */
+		offset = addr & ~PAGE_MASK;
 
-	/* align address to page boundary */
-	offset = addr & ((1UL << shift) - 1);
-
-	pte = READ_ONCE(*ptep);
-	if (!pte_present(pte) || !pte_user(pte))
-		goto err_out;
-	pfn = pte_pfn(pte);
-	if (!page_is_ram(pfn))
-		goto err_out;
-
-	/* no highmem to worry about here */
-	kaddr = pfn_to_kaddr(pfn);
-	memcpy(buf, kaddr + offset, nb);
-	ret = 0;
-err_out:
-	local_irq_restore(flags);
-	return ret;
+		memcpy(buf, kaddr + offset, nb);
+		put_page(page);
+		return 0;
+	}
+	return -EFAULT;
 }
 
 static int read_user_stack_64(unsigned long __user *ptr, unsigned long *ret)
