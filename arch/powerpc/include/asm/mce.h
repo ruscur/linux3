0cb0ecf35a64554880d63afe4b6b09e94e07ab9e
Santosh S <santosh@fossix.org>
2020-03-30 12:42:19 +0530

    powerpc/mce: Add MCE notification chain
    Introduce notification chain which lets know about uncorrected memory
    errors(UE). This would help prospective users in pmem or nvdimm subsystem
    to track bad blocks for better handling of persistent memory allocations.
    
    Signed-off-by: Santosh S <santosh@fossix.org>
    Signed-off-by: Ganesh Goudar <ganeshgr@linux.ibm.com>

--- arch/powerpc/include/asm/mce.h
+++ arch/powerpc/include/asm/mce.h
@@ -218,6 +218,8 @@ extern void machine_check_queue_event(void);
 extern void machine_check_print_event_info(struct machine_check_event *evt,
 					   bool user_mode, bool in_guest);
 unsigned long addr_to_pfn(struct pt_regs *regs, unsigned long addr);
+int mce_register_notifier(struct notifier_block *nb);
+int mce_unregister_notifier(struct notifier_block *nb);
 #ifdef CONFIG_PPC_BOOK3S_64
 void flush_and_reload_slb(void);
 #endif /* CONFIG_PPC_BOOK3S_64 */
