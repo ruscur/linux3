7157314bf0c7ddfa228b6ad791470273d6be09db
Christophe Leroy <christophe.leroy@c-s.fr>
2020-03-31 07:49:54 +0000

    powerpc/platforms: Move files from 4xx to 44x
    Only 44x uses 4xx now, so only keep one directory.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>

--- arch/powerpc/platforms/44x/machine_check.c
+++ arch/powerpc/platforms/44x/machine_check.c
@@ -8,6 +8,20 @@
 
 #include <asm/reg.h>
 
+int machine_check_4xx(struct pt_regs *regs)
+{
+	unsigned long reason = regs->dsisr;
+
+	if (reason & ESR_IMCP) {
+		printk("Instruction");
+		mtspr(SPRN_ESR, reason & ~ESR_IMCP);
+	} else
+		printk("Data");
+	printk(" machine check in kernel mode.\n");
+
+	return 0;
+}
+
 int machine_check_440A(struct pt_regs *regs)
 {
 	unsigned long reason = regs->dsisr;
