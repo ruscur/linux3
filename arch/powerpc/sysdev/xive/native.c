ea31b9744b95fb47e1a052ee85c00ff5924866ea
Qian Cai <cai@lca.pw>
2020-05-08 21:55:38 -0400

    powerpc/kvm: silence kmemleak false positives
    kvmppc_pmd_alloc() and kvmppc_pte_alloc() allocate some memory but then
    pud_populate() and pmd_populate() will use __pa() to reference the newly
    allocated memory. The same is in xive_native_provision_pages().
    
    Since kmemleak is unable to track the physical memory resulting in false
    positives, silence those by using kmemleak_ignore().
    
    unreferenced object 0xc000201c382a1000 (size 4096):
      comm "qemu-kvm", pid 124828, jiffies 4295733767 (age 341.250s)
      hex dump (first 32 bytes):
        c0 00 20 09 f4 60 03 87 c0 00 20 10 72 a0 03 87  .. ..`.... .r...
        c0 00 20 0e 13 a0 03 87 c0 00 20 1b dc c0 03 87  .. ....... .....
      backtrace:
        [<000000004cc2790f>] kvmppc_create_pte+0x838/0xd20 [kvm_hv]
        kvmppc_pmd_alloc at arch/powerpc/kvm/book3s_64_mmu_radix.c:366
        (inlined by) kvmppc_create_pte at arch/powerpc/kvm/book3s_64_mmu_radix.c:590
        [<00000000d123c49a>] kvmppc_book3s_instantiate_page+0x2e0/0x8c0 [kvm_hv]
        [<00000000bb549087>] kvmppc_book3s_radix_page_fault+0x1b4/0x2b0 [kvm_hv]
        [<0000000086dddc0e>] kvmppc_book3s_hv_page_fault+0x214/0x12a0 [kvm_hv]
        [<000000005ae9ccc2>] kvmppc_vcpu_run_hv+0xc5c/0x15f0 [kvm_hv]
        [<00000000d22162ff>] kvmppc_vcpu_run+0x34/0x48 [kvm]
        [<00000000d6953bc4>] kvm_arch_vcpu_ioctl_run+0x314/0x420 [kvm]
        [<000000002543dd54>] kvm_vcpu_ioctl+0x33c/0x950 [kvm]
        [<0000000048155cd6>] ksys_ioctl+0xd8/0x130
        [<0000000041ffeaa7>] sys_ioctl+0x28/0x40
        [<000000004afc4310>] system_call_exception+0x114/0x1e0
        [<00000000fb70a873>] system_call_common+0xf0/0x278
    unreferenced object 0xc0002001f0c03900 (size 256):
      comm "qemu-kvm", pid 124830, jiffies 4295735235 (age 326.570s)
      hex dump (first 32 bytes):
        c0 00 20 10 fa a0 03 87 c0 00 20 10 fa a1 03 87  .. ....... .....
        c0 00 20 10 fa a2 03 87 c0 00 20 10 fa a3 03 87  .. ....... .....
      backtrace:
        [<0000000023f675b8>] kvmppc_create_pte+0x854/0xd20 [kvm_hv]
        kvmppc_pte_alloc at arch/powerpc/kvm/book3s_64_mmu_radix.c:356
        (inlined by) kvmppc_create_pte at arch/powerpc/kvm/book3s_64_mmu_radix.c:593
        [<00000000d123c49a>] kvmppc_book3s_instantiate_page+0x2e0/0x8c0 [kvm_hv]
        [<00000000bb549087>] kvmppc_book3s_radix_page_fault+0x1b4/0x2b0 [kvm_hv]
        [<0000000086dddc0e>] kvmppc_book3s_hv_page_fault+0x214/0x12a0 [kvm_hv]
        [<000000005ae9ccc2>] kvmppc_vcpu_run_hv+0xc5c/0x15f0 [kvm_hv]
        [<00000000d22162ff>] kvmppc_vcpu_run+0x34/0x48 [kvm]
        [<00000000d6953bc4>] kvm_arch_vcpu_ioctl_run+0x314/0x420 [kvm]
        [<000000002543dd54>] kvm_vcpu_ioctl+0x33c/0x950 [kvm]
        [<0000000048155cd6>] ksys_ioctl+0xd8/0x130
        [<0000000041ffeaa7>] sys_ioctl+0x28/0x40
        [<000000004afc4310>] system_call_exception+0x114/0x1e0
        [<00000000fb70a873>] system_call_common+0xf0/0x278
    unreferenced object 0xc000201b53e90000 (size 65536):
      comm "qemu-kvm", pid 124557, jiffies 4295650285 (age 364.370s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        [<00000000acc2fb77>] xive_native_alloc_vp_block+0x168/0x210
        xive_native_provision_pages at arch/powerpc/sysdev/xive/native.c:645
        (inlined by) xive_native_alloc_vp_block at arch/powerpc/sysdev/xive/native.c:674
        [<000000004d5c7964>] kvmppc_xive_compute_vp_id+0x20c/0x3b0 [kvm]
        [<0000000055317cd2>] kvmppc_xive_connect_vcpu+0xa4/0x4a0 [kvm]
        [<0000000093dfc014>] kvm_arch_vcpu_ioctl+0x388/0x508 [kvm]
        [<00000000d25aea0f>] kvm_vcpu_ioctl+0x15c/0x950 [kvm]
        [<0000000048155cd6>] ksys_ioctl+0xd8/0x130
        [<0000000041ffeaa7>] sys_ioctl+0x28/0x40
        [<000000004afc4310>] system_call_exception+0x114/0x1e0
        [<00000000fb70a873>] system_call_common+0xf0/0x278
    
    Signed-off-by: Qian Cai <cai@lca.pw>

--- arch/powerpc/sysdev/xive/native.c
+++ arch/powerpc/sysdev/xive/native.c
@@ -18,6 +18,7 @@
 #include <linux/delay.h>
 #include <linux/cpumask.h>
 #include <linux/mm.h>
+#include <linux/kmemleak.h>
 
 #include <asm/machdep.h>
 #include <asm/prom.h>
@@ -647,6 +648,9 @@ static bool xive_native_provision_pages(void)
 			pr_err("Failed to allocate provisioning page\n");
 			return false;
 		}
+		/* Kmemleak is unable to track the physical address. */
+		kmemleak_ignore(p);
+
 		opal_xive_donate_page(chip, __pa(p));
 	}
 	return true;
