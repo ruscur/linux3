5b39e9eb2d4742e2936518d86de71da4bf558d22
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2020-06-12 19:59:53 +0530

    powerpc/pseries/svm: Drop unused align argument in alloc_shared_lppaca() function
    Argument "align" in alloc_shared_lppaca() was unused inside the
    function. Let's drop it and update code comment for page alignment.
    
    Cc: linux-kernel@vger.kernel.org
    Cc: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Cc: Ram Pai <linuxram@us.ibm.com>
    Cc: Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
    Cc: Laurent Dufour <ldufour@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

--- arch/powerpc/kernel/paca.c
+++ arch/powerpc/kernel/paca.c
@@ -57,8 +57,8 @@ static void *__init alloc_paca_data(unsigned long size, unsigned long align,
 
 #define LPPACA_SIZE 0x400
 
-static void *__init alloc_shared_lppaca(unsigned long size, unsigned long align,
-					unsigned long limit, int cpu)
+static void *__init alloc_shared_lppaca(unsigned long size, unsigned long limit,
+					int cpu)
 {
 	size_t shared_lppaca_total_size = PAGE_ALIGN(nr_cpu_ids * LPPACA_SIZE);
 	static unsigned long shared_lppaca_size;
@@ -68,6 +68,12 @@ static void *__init alloc_shared_lppaca(unsigned long size, unsigned long align,
 	if (!shared_lppaca) {
 		memblock_set_bottom_up(true);
 
+		/* See Documentation/powerpc/ultravisor.rst for mode details
+		 *
+		 * UV/HV data share is in PAGE granularity, In order to
+		 * minimize the number of pages shared and maximize the
+		 * use of a page, let's use page align.
+		 */
 		shared_lppaca =
 			memblock_alloc_try_nid(shared_lppaca_total_size,
 					       PAGE_SIZE, MEMBLOCK_LOW_LIMIT,
@@ -122,7 +128,7 @@ static struct lppaca * __init new_lppaca(int cpu, unsigned long limit)
 		return NULL;
 
 	if (is_secure_guest())
-		lp = alloc_shared_lppaca(LPPACA_SIZE, 0x400, limit, cpu);
+		lp = alloc_shared_lppaca(LPPACA_SIZE, limit, cpu);
 	else
 		lp = alloc_paca_data(LPPACA_SIZE, 0x400, limit, cpu);
 
