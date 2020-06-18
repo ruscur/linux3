4cb797a1da0695596277c12c2213060d913576f6
Prakhar Srivastava <prsriva@linux.microsoft.com>
2020-06-18 00:10:43 -0700

    Refactoring powerpc code for carrying over IMA measurement logs, to move non architecture specific code to security/ima.
    Powerpc has support to carry over the IMA measurement logs. Refatoring the
    non-architecture specific code out of arch/powerpc and into security/ima.
    
    The code adds support for reserving and freeing up of memory for IMA measurement
    logs.

--- arch/powerpc/include/asm/ima.h
+++ arch/powerpc/include/asm/ima.h
@@ -4,15 +4,6 @@
 
 struct kimage;
 
-int ima_get_kexec_buffer(void **addr, size_t *size);
-int ima_free_kexec_buffer(void);
-
-#ifdef CONFIG_IMA
-void remove_ima_buffer(void *fdt, int chosen_node);
-#else
-static inline void remove_ima_buffer(void *fdt, int chosen_node) {}
-#endif
-
 #ifdef CONFIG_IMA_KEXEC
 int arch_ima_add_kexec_buffer(struct kimage *image, unsigned long load_addr,
 			      size_t size);
@@ -22,7 +13,6 @@ int setup_ima_buffer(const struct kimage *image, void *fdt, int chosen_node);
 static inline int setup_ima_buffer(const struct kimage *image, void *fdt,
 				   int chosen_node)
 {
-	remove_ima_buffer(fdt, chosen_node);
 	return 0;
 }
 #endif /* CONFIG_IMA_KEXEC */
