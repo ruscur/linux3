bb62a4774f27bd5d56ac8a9cfc5b98cb593e3f5d
Christophe Leroy <christophe.leroy@csgroup.eu>
2020-08-12 12:06:00 +0000

    powerpc/signal: Call get_tm_stackpointer() from get_sigframe()
    Instead of calling get_tm_stackpointer() from the caller, call it
    directly from get_sigframe(). This avoids a double call and
    allows get_tm_stackpointer() to become static and be inlined
    into get_sigframe() by GCC.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>

--- arch/powerpc/kernel/signal_64.c
+++ arch/powerpc/kernel/signal_64.c
@@ -824,7 +824,7 @@ int handle_rt_signal64(struct ksignal *ksig, sigset_t *set,
 
 	BUG_ON(tsk != current);
 
-	frame = get_sigframe(ksig, get_tm_stackpointer(tsk), sizeof(*frame), 0);
+	frame = get_sigframe(ksig, tsk, sizeof(*frame), 0);
 	if (!access_ok(frame, sizeof(*frame)))
 		goto badframe;
 
